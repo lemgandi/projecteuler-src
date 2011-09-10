c526d063a826daa63ce16554d314da603a7920cd
Charles Shapiro <devel@piglet.(none)>
2011-09-10 08:32:41 -0400

    Add binary search to determine if a number is in fact part of the abundant number table.

--- problem23.lua
+++ problem23.lua
@@ -2,9 +2,40 @@
 -- Solve problem 23
 --
 require "useful"
+require "math"
+--
+-- Binary search sorted table
+--
+function bin_search(n,table,first,last)
+   if first > last then
+      retVal=nil
+   else 
+      mid=math.floor((first+last)/2)
+      if n == table[mid] then
+         retVal=mid
+      elseif n < table[mid] then
+         bin_search(n,table,first,mid-1)
+      else
+         bin_search(n,table,mid+1,last)
+      end
+   end
+   return retVal
+end
 
-max_noncomposite_sum=28123
--- max_noncomposite_sum=100
+--
+-- Does a given number appear in the abundant table?
+--
+function is_abundant(n,abundant_table)
+   local found=bin_search(n,abundant_table,1,#abundant_table)
+   retVal=true
+   if nil == found then
+      retVal=false
+   end
+   return retVal
+end
+
+-- max_noncomposite_sum=28123
+max_noncomposite_sum=100
 
 abundant_numbers={}
 
@@ -15,6 +46,18 @@ for kk=1,max_noncomposite_sum do
       abundant_numbers[#abundant_numbers+1]=kk
    end
 end
+
 useful.print_array(abundant_numbers)
 print("Abundant numbers below " .. max_noncomposite_sum .. ":" .. #abundant_numbers .. "")
+sum_numbers={}
+for kk=1,max_noncomposite_sum do
+   sum_numbers[kk]=kk
+end
+
+for jj,kk in ipairs(sum_numbers) do
+   if is_abundant(kk,abundant_numbers) then
+      sum_numbers[jj]=0
+   end   
+end
+useful.print_array(sum_numbers)
 
