d78e9ce2cb9baf3ced1955e4a51f5eda6a3c8d66
Charles Shapiro <devel@piglet.(none)>
2011-09-22 16:43:34 -0400

    Solve problem 23

--- problem23.lua
+++ problem23.lua
@@ -16,7 +16,38 @@ function is_abundant(n,abundant_table)
    return retVal
 end
 
-max_noncomposite_sum=28123
+-- 
+-- Can given number be expressed as a sum of two abundant numbers?
+--
+function is_abundant_sum_of_two(n,abundant_table)
+
+   local kk=1
+   local retVal=0
+   local nsub=0
+
+   while 0 == retVal and abundant_table[kk] <= math.ceil(n/2)
+   do
+      nsub=n-abundant_table[kk]
+      if is_abundant(nsub,abundant_table)
+      then
+         retVal=1      
+      end
+      kk=kk+1        
+      if kk > #abundant_table then
+         retVal=-1
+      end
+   end
+   if 0 == retVal then
+      retVal=-1
+   end
+   return retVal
+end
+
+--
+-- Main Line
+--
+
+max_noncomposite_sum=20161
 -- max_noncomposite_sum=100
 
 abundant_numbers={}
@@ -34,18 +65,29 @@ print("Abundant numbers below " .. max_noncomposite_sum .. ":" .. #abundant_numb
 --[[
 print("7 is abundant: ",is_abundant(7,abundant_numbers))
 print("18 is abundant: ", is_abundant(18,abundant_numbers))
---]]
-
+]]--
 
 sum_numbers={}
 for kk=1,max_noncomposite_sum do
    sum_numbers[kk]=kk
 end
 
-for jj,kk in ipairs(sum_numbers) do
-   if is_abundant(kk,abundant_numbers) then
-      sum_numbers[jj]=0
-   end   
+print("is_abundant_sum_of_two(28123,abundant_numbers)",is_abundant_sum_of_two(28123,abundant_numbers))
+
+
+print("#sum_numbers:",#sum_numbers)
+
+for kk=1,#sum_numbers do
+   if 1 == is_abundant_sum_of_two(sum_numbers[kk],abundant_numbers)
+   then
+      sum_numbers[kk] = 0
+   end
 end
-print("Abundant_numbers are 0 in this array:")
+
+
+print("Sums of Abundant_numbers are 0 in this array:")
 useful.print_array(sum_numbers)
+
+
+sum=useful.sumtable(sum_numbers)
+print("Answer:",sum)
