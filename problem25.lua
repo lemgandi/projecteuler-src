feb881c0b551e9750e97e5f983f75278ab6000ac
Charles Shapiro <devel@piglet.(none)>
2011-10-01 16:52:16 -0400

    Problem 25 solved

---
+++ problem25.lua
@@ -0,0 +1,26 @@
+--[[
+   Find the first term in the fibonacci sequence to contain 1,000 digits
+]]--
+
+require "math"
+require "useful"
+require "bignum"
+require "table"
+
+
+--
+--  Main Line
+--
+
+
+fibtable={bignum.new(0),bignum.new(1)}
+limit = bignum.new(1000)
+counter=1
+while fibtable[2]:len_digits() ~= limit
+do
+   counter = counter + 1
+   n=fibtable[1]+fibtable[2]
+   fibtable[1] = fibtable[2]
+   fibtable[2]=n
+end
+print(counter)
